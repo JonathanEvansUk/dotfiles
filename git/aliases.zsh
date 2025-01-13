74170cb26b91b2a540a92d4e4a919bfca5a7ed01
Jonathan Evans <jonathan.evans@paytrix.io>
2025-01-13 15:35:58 +0000

    git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,7 +9,7 @@ fi
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-#alias gp='git push origin HEAD'
+alias gpuoh='git push -u origin HEAD'
 alias gp='git pull'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
@@ -19,8 +19,12 @@ alias gcm='git checkout main'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
+alias gcob='git checkout -b'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+
+# git prune branches
+alias gpb='git fetch --prune && git branch -r | awk "{print \$1}" | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk "{print \$1}" | xargs git branch -D'
